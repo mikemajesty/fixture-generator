e71da21c96f6ddd3de1469f20e0b3abf472fe551
mikemajesty <mikemajesty2011@gmail.com>
2018-08-25 11:59:58 -0300

    add more features

--- src/services/fixture.js
+++ src/services/fixture.js
@@ -1,5 +1,19 @@
 import acessApi from './acess-api';
 
-export default function fixture() {
-  return acessApi();
+export default function fixture(res) {
+  return acessApi().then(list => {
+    const filter = [{ property: 'repositories.full', length: 1 }];
+    let properties = [];
+    filter.forEach(f => {
+      f.property.split('.').map((p) => {
+        properties.push([p]);
+      });
+
+      properties.forEach(a => {
+        console.log(a);
+      })
+      
+      //console.log(list + f.property)
+    });
+  });
 };
